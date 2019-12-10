8c9028c9654f34e9fe1ba09621aa19788e5cf7c6
Арина Сырченко <syrchenkoarina@gmail.com>
2019-12-10 15:04:39 +0300

    basic task

--- server.py
+++ server.py
@@ -1,32 +1,23 @@
 import socket
 
-
+port = int(input("РџРѕСЂС‚:"))
 sock = socket.socket()
-
 try:
-    sock.bind(('', 80))
-    print("Using port 80")
+    sock.bind(('', port))
 except OSError:
     sock.bind(('', 8080))
-    print("Using port 8080")
-
-sock.listen(5)
 
 conn, addr = sock.accept()
 print("Connected", addr)
-
 data = conn.recv(8192)
 msg = data.decode()
-
-print(msg)
-
-resp = """HTTP/1.1 200 OK
-Server: SelfMadeServer v0.0.1
-Content-type: text/html
-Connection: close
-
-Hello, webworld!"""
-
+name = msg.split()[1][1:]
+if name == "" : name = "1.html"
+resp = f"""HTTP/1.1 200 OK
+    Connection: close
+    
+    """
+with open(name, "r") as f:
+    resp += f.read()
 conn.send(resp.encode())
-
 conn.close()
